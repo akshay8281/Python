ea6f53b258aa74285d03eba6e326072889e11b98
Akshay <akshaypitroda4@gmail.com>
2024-04-09 22:20:12 +0530

    files

---
+++ 4. StarPattern.py
@@ -0,0 +1,17 @@
+print("Low TO High Star Pattern")
+
+for i in range (1,15) :
+    print("*" * i)
+
+print("High to Low Star Pattern")
+
+for i in range (10,1) :
+    print("*"  * i)
+
+print("Pyramid Star Pattern")
+
+for i in range (1,10) :
+    print(" "*(9-i)," *" * i)
+
+
+
---
+++ 5. ForLoop.py
@@ -0,0 +1,17 @@
+print("For Loop Concept in Python : ")
+
+for i in range (11):
+    print(i)
+print("*************")
+
+for i in range (0,11):
+    print(i)
+print("*************")
+
+for i in range (0,11,2):
+    print(i)
+print("*************")
+
+for i in range (11,0,-2):
+    print(i)
+print("*************")#For Comments in Python
