8532b3f12fd8fc47455f6279f82eaa320d1b62dc
AP <134692290+akshay8281@users.noreply.github.com>
2024-04-11 21:40:00 +0530

    Add files via upload

---
+++ whileLoop.py
@@ -0,0 +1,50 @@
+#Introduction
+'''
+n = int(input("Enter  number of N : "))
+
+while n>0 :
+    print("My Way")
+    n = n-1
+'''
+
+#Sum of Total integers
+'''
+n = int(input("Enter a Number : "))
+
+sum = 0
+while n>0 :
+    sum = sum + n
+    n = n - 1
+print("Sum : ", sum)
+'''
+
+'''
+for i in range (1,10) :
+
+    if i ==6 :
+        break
+    else :
+        print("I is", i)
+'''
+# For Loop using Continue statement
+'''
+for i in range (1,10) :
+
+    if i ==6 :
+        continue
+    else :
+        print("I is", i)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
