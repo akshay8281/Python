0b333c98de3d2ae66753bc2464f612c69966680b
AP <134692290+akshay8281@users.noreply.github.com>
2024-04-13 20:16:18 +0530

    Add files via upload

---
+++ StringIndexCount.py
@@ -0,0 +1,22 @@
+#    01234567890123456
+s = "Tops Technologies"
+#    76543210987654321
+
+# Positive Index integer
+
+print(s[3:13])
+print(s[:15])
+print(s[2:])
+print(s[3:15:3])
+print(s[::5])
+
+# Negative Index integer
+s = "Tops Technologies"
+#    76543210987654321
+
+print(s[-16:-3])
+print(s[:-5])
+print(s[-4:])
+print(s[-17:-2:2])
+print(s[::-3])
+
