9988a8755d3dd7752a545800d2804187117a2560
AP <134692290+akshay8281@users.noreply.github.com>
2024-04-13 20:16:35 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,28 @@
+'''
+a = "Tops Technologies"
+print(len(a))
+print(a)
+print(a.capitalize())
+print(a.casefold())
+print(a.center(40, "*"))
+print(a.count("T"))
+print(a.find("T"))
+print(a.index("T",2))
+print(a.lower())
+print(a.upper())
+print(a.startswith("To"))
+print("Tops123".isalnum())
+print("123".isalnum())
+print("Tops".isalnum())
+print("Tops".isalpha())
+print("Tops".isnumeric())
+print(" ".isspace())
+print("My Name is Akshay".istitle())
+print("Tops".islower())
+print("Tops".isupper())
+print(a.replace("o", "O"))
+'''
+
+
+
+
